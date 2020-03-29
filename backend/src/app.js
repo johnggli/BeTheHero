6dad4c4452249185f50ac67c8f6509db067aed6b
john-emerson14 <john.carv.sousa@gmail.com>
2020-03-29 14:42:24 -0300

    chore: add validation & tests

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,15 +2,19 @@ const express = require('express'); /* pacote */
 const routes = require('./routes'); /* arquivo */
 const cors = require('cors');
 
+const { errors } = require('celebrate');
+
 const app = express();
 
 app.use(cors()); /* todas as aplicações frontends podem acessar */
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
 /**
  * Driver: SELECT * FROM USERS
  * Query Builder: table('users').select('*').where()
  */
 
-app.listen(3333);
+// app.listen(3333);
+module.exports = app;
